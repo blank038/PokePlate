3ce76bb039f4f69bd641de72aae324cd13896917
Blank038 <admin@mc9y.com>
2025-09-03 02:31:48 +0800

    feat: Implement placeholder expansion

--- common/src/main/java/com/aiyostudio/pokeplate/hook/PlaceholderHook.java
+++ common/src/main/java/com/aiyostudio/pokeplate/hook/PlaceholderHook.java
@@ -1,8 +1,11 @@
 package com.aiyostudio.pokeplate.hook;
 
 import com.aiyostudio.pokeplate.PokePlate;
+import com.aiyostudio.pokeplate.data.plate.PlateData;
 import com.aiyostudio.pokeplate.data.player.PlayerData;
 import com.aiyostudio.pokeplate.data.DataContainer;
+import com.aiyostudio.pokeplate.data.player.PokedexChildData;
+import com.aiyostudio.pokeplate.storage.StorageHandler;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 
@@ -33,17 +36,31 @@ public class PlaceholderHook extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player p, String params) {
-//        if (DataContainer.PLAYER_DATA_MAP.containsKey(p.getName())) {
-//            PlayerData playerData = DataContainer.PLAYER_DATA_MAP.get(p.getName());
-//            switch (params) {
-//                case "count":
-//                    return String.valueOf(playerData.getCount());
-//                case "pg":
-//                    return String.valueOf(playerData.getProgress() * 100);
-//                default:
-//                    break;
-//            }
-//        }
-        return null;
+        if (p == null || !p.isOnline()) {
+            return "";
+        }
+        PlayerData playerData = StorageHandler.getPlayerDataFromMemory(p.getName()).orElse(null);
+        if (playerData == null) {
+            return "";
+        }
+        String[] split = params.split("_");
+        if (split.length == 1) {
+            return "ERROR";
+        }
+        PlateData plateData = DataContainer.PLATE_DATA.get(split[1]);
+        if (plateData == null) {
+            return "NOT_FOUND_PLATE";
+        }
+        PokedexChildData childData = playerData.getChild(split[1]);
+        switch (split[0]) {
+            case "count":
+                return String.valueOf(childData.getCount());
+            case "pg":
+                int progress = (int) ((double) childData.getCount() / plateData.getRequire().getPokemon().size() * 100);
+                return String.valueOf(progress);
+            default:
+                break;
+        }
+        return "";
     }
 }
